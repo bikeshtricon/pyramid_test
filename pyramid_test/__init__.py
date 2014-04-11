48929077855b88a8e0f11f19181c3915b9010050
bikeshtricon <bikesh@triconinfotech.com>
2014-04-11 11:28:25 +0530

    added page 3

--- pyramid_test/__init__.py
+++ pyramid_test/__init__.py
@@ -1,8 +1,8 @@
 from pyramid.config import Configurator
 
-
+#Added a comment by Vijay mearged
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
+    """ This function returns a Pyramid WSGI application...
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
@@ -10,6 +10,9 @@ def main(global_config, **settings):
     config.add_route('home', '/')
     
     config.add_route('page2', '/page2')
+    
+    
+    #config.add_route('page3', '/page3')
     
     
     config.scan()
